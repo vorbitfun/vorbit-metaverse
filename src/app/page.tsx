82cb0dcbd6c343ec5d2605b37f0d261675a842e9
vorbitfun <amarredli5@gmail.com>
2026-06-19 14:49:16 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Workflow from "@/components/Workflow";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Workflow />
+    </>
+  );
+}
